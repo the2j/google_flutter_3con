3b0dc9c2fc1ebfe7183a3432a316dacf2580a3b0
Jordan Carter <jordan.acarter1999@gmail.com>
2021-10-20 22:09:32 +1000

    removed repeated manual modulus add to final innerData

--- lib/Googletesting/api_controller/google/google_data.dart
+++ lib/Googletesting/api_controller/google/google_data.dart
@@ -4,6 +4,8 @@
 
 import 'dart:ffi';
 
+import 'package:flutter/material.dart';
+
 import '../unified_data.dart';
 import 'google_types.dart';
 import 'package:googleapis/fitness/v1.dart';
@@ -30,99 +32,104 @@ class GoogleHealthData extends UnifiedHealthData {
   }
 
 
-  //
-  List<dynamic> datasetAsList(Dataset data) {
+  ///RETURN DATA AS FINAL 1 LIST AS WELL AS WELL AS SEND TO INNER DATA LIST
+  ///
+  ///
+  /// [data] fitness dataset - contains health values for request and other information
+  /// [wekType] name of the health data type that is to be passed to within data_types
+  /// [indx] if multiple values are returned for a datapoint, get the required values index
+  List<dynamic> datasetAsList(Dataset data,String wekoType, [int indx = -1]) {
+
     var valuesList = <dynamic>[];
     data.point!.forEach((datapoint) {
+      int pos = 0;
       datapoint.value!.forEach((datavalue) {
-        if(datavalue.fpVal != null) valuesList.add(datavalue.fpVal!);
-        else  {
-          if (datavalue.intVal != null) valuesList.add(datavalue.intVal!);
+        if (indx == -1 || indx == pos) {
+          //gets either intger value or floating point value
+          if (datavalue.fpVal != null)
+            valuesList.add(datavalue.fpVal!);
+          else {
+            if (datavalue.intVal != null) valuesList.add(datavalue.intVal!);
+          }
         }
+        pos++;
       });
+
   });
+    //add data to innerData map that will be sent to weko
+    for (int i = 0; i < valuesList.length; i++) {
+      innerData[wekoType].add(valuesList[i]);
+    }
+    //add values to innerdata to transfer to weko
     return valuesList;
   }
+
   ///
   /// Manages data for each individual health data type
+  ///
   void addData(Dataset data) {
     var dataValues = <dynamic>[];
     String? dataType = data.dataSourceId;
       //get data in the correct format - so basically it is just the right values within a list
       switch(dataType) {
         case "derived:com.google.weight:com.google.android.gms:merge_weight":  //WEIGHT
-          var valuesList = <dynamic>[];
-          valuesList = datasetAsList(data);
-
           //SEND DATA valuesList TO WEKO
           String dataname = "weight";
-          for (int i = 0; i < valuesList.length; i++) {
-            innerData[dataname].add(valuesList[i]);
-          }
-          //innerData[dataname].add(valuesList);
+          var valuesList = <dynamic>[];
+          valuesList = datasetAsList(data, dataname);
+
           print("weight: " + valuesList.toString());
           break;
 
         case "derived:com.google.height:com.google.android.gms:merge_height":  //HEIGHT
-          var valuesList = <dynamic>[];
-          valuesList = datasetAsList(data);
-
           //SEND DATA valuesList TO WEKO
           String dataname = "height";
-          for (int i = 0; i < valuesList.length; i++) {
-            innerData[dataname].add(valuesList[i]);
-          }
-          //innerData[dataname].add(valuesList);
+          var valuesList = <dynamic>[];
+          valuesList = datasetAsList(data, dataname);
+
           print("height: " + valuesList.toString());
           break;
 
         case "derived:com.google.step_count.delta:com.google.android.gms:merge_step_deltas": //STEPS
           var valuesList = <dynamic>[];
-          valuesList = datasetAsList(data);
-
           //SEND DATA valuesList TO WEKO
           String dataname = "steps";
-          for (int i = 0; i < valuesList.length; i++) {
-            innerData[dataname].add(valuesList[i]);
-          }
-          //innerData[dataname].add(valuesList);
+          valuesList = datasetAsList(data,dataname);
           print("steps: " + valuesList.toString());
           break;
 
         case "derived:com.google.blood_pressure:com.google.android.gms:merged": //BLOOD PRESSURE
-          var valuesList = <dynamic>[];
-          valuesList = datasetAsList(data);
-
-          //SEND DATA valuesList TO WEKO
+        //SEND DATA valuesList TO WEKO
           String dataname = "bloodPressureDiastolic";
-          for (int i = 0; i < valuesList.length; i++) {
-            if (i == 0 || i % 4 == 0) {
-              innerData["bloodPressureSystolic"].add(valuesList[i]);
-            }
-            else if ( i % 4 == 1)
-              {
-                innerData["bloodPressureDiastolic"].add(valuesList[i]);
-              }
-          }
+          var valuesList = <dynamic>[];
+          valuesList = datasetAsList(data, dataname, 0);
+          // for (int i = 0; i < valuesList.length; i++) {
+          //   if (i == 0 || i % 4 == 0) {
+          //     innerData["bloodPressureSystolic"].add(valuesList[i]);
+          //   }
+          //   else if ( i % 4 == 1)
+          //     {
+          //       innerData["bloodPressureDiastolic"].add(valuesList[i]);
+          //     }
+          // }
           //innerData[dataname].add(valuesList);
           print("blood pressure: " + valuesList.toString());
           break;
 
         case "derived:com.google.oxygen_saturation:com.google.android.gms:merged": //OXYGEN
-          var valuesList = <dynamic>[];
-          valuesList = datasetAsList(data);
-          //SEND DATA valuesList TO WEKO
-          //latest & average
+        //SEND DATA valuesList TO WEKO
+        //latest & average
+        //BLOOD SATURATION
           String dataname = "bloodOxygen";
-          for (int i = 0; i < valuesList.length; i++) {
-            if(i == 0 || i % 5 == 0){
-              innerData[dataname].add(valuesList[i]);
-            }
-          }
+          var valuesList = <dynamic>[];
+          valuesList = datasetAsList(data, dataname, 0);
+          // for (int i = 0; i < valuesList.length; i++) {
+          //   if(i == 0 || i % 5 == 0){
+          //     innerData[dataname].add(valuesList[i]);
+          //   }
+          // }
           //innerData[dataname].add(valuesList[0]);
-
           print("oxygen: " + valuesList.toString());
-
           //valuesList[0] is saturation
           //.,..
 
@@ -130,32 +137,27 @@ class GoogleHealthData extends UnifiedHealthData {
 
         case "derived:com.google.heart_rate.bpm:com.google.android.gms:merge_heart_rate_bpm": //HEART RATE
           var valuesList = <dynamic>[];
-          valuesList = datasetAsList(data);
-
           //SEND DATA valuesList TO WEKO
           String dataname = "heartRate";
-          for (int i = 0; i < valuesList.length; i++) {
-            innerData[dataname].add(valuesList[i]);
-          }
-          //innerData[dataname].add(valuesList);
+          valuesList = datasetAsList(data, dataname);
           print("heartRate: " + valuesList.toString());
           break;
 
         case "derived:com.google.body.temperature:com.google.android.gms:merged": //BODY TEMPERATURE
-          var valuesList = <dynamic>[];
-          valuesList = datasetAsList(data);
-
           //SEND DATA valuesList TO WEKO
           String dataname = "bodyTemperature";
-          for (int i = 0; i < valuesList.length; i++) {
-            if(i == 0 || i % 2 == 0){
-              innerData[dataname].add(valuesList[i]);
-            }
-          }
+          var valuesList = <dynamic>[];
+          valuesList = datasetAsList(data, dataname, 0);
+          // for (int i = 0; i < valuesList.length; i++) {
+          //   if(i == 0 || i % 2 == 0){
+          //     innerData[dataname].add(valuesList[i]);
+          //   }
+          // }
 
           //innerData[dataname].add(valuesList);
           print("bodyTemperature: " + valuesList.toString());
           break;
+
         }
         print(innerData.toString());
     }
